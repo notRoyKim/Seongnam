d9768f41af122e00c3bf00ada4e70191e8e2a68f
notRoyKim <corgyeier@gmail.com>
2020-09-16 17:10:52 +0900

    build test 완료, 초반 dependency 에러 수정 완료

---
+++ src/main/java/com/CICS/Seongnam/Controller/DispatcherCont/Main/MainController.java
@@ -0,0 +1,27 @@
+package com.CICS.Seongnam.Controller.DispatcherCont.Main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView base(HttpServletRequest request) {
+        ModelAndView mv = new ModelAndView();
+
+        return mv;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test(HttpServletRequest request) {
+        String hello = "hello world";
+        return hello;
+    }
+}
