3ecff2707e0d6cfcb727e3b823c347d0e4d8312f
NotRoyKim <sinant7616@gmail.com>
2020-09-16 16:24:22 +0900

    initial commit

---
+++ src/main/java/com/CICS/Seongnam/SeongnamApplication.java
@@ -0,0 +1,13 @@
+package com.CICS.Seongnam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeongnamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeongnamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CICS/Seongnam/SeongnamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CICS.Seongnam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeongnamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
