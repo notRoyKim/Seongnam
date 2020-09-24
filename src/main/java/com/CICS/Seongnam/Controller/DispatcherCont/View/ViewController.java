ed0477d5fb41dd23a7ba0386f329c84d8e43510e
notRoyKim <corgyeier@gmail.com>
2020-09-24 13:40:27 +0900

    viewpage 기능추가중

--- src/main/java/com/CICS/Seongnam/Controller/DispatcherCont/View/ViewController.java
+++ src/main/java/com/CICS/Seongnam/Controller/DispatcherCont/View/ViewController.java
@@ -2,9 +2,7 @@ package com.CICS.Seongnam.Controller.DispatcherCont.View;
 
 import com.CICS.Seongnam.Domain.ViewData;
 import com.CICS.Seongnam.Service.View.ViewService;
-import org.apache.ibatis.jdbc.Null;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.spel.ast.NullLiteral;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +11,6 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class ViewController {
