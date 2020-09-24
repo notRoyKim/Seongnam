804ca90bd99010ad3a7f4b2982c0da156304cd64
NotRoyKim <sinant7616@gmail.com>
2020-09-24 17:37:30 +0900

    viewpage test중

--- src/test/java/com/CICS/Seongnam/SeongnamApplicationTests.java
+++ src/test/java/com/CICS/Seongnam/SeongnamApplicationTests.java
@@ -3,7 +3,6 @@ package com.CICS.Seongnam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SeongnamApplicationTests {
 
 	@Test
