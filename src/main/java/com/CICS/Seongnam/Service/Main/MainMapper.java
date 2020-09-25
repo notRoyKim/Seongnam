1021b8b5827f49b581f575da58da4c678749bdc1
notRoyKim <corgyeier@gmail.com>
2020-09-25 17:37:58 +0900

    base 일단 기존 사이트 디자인 그대로 가져오고, view페이지 이미지 가져오기 작업 시작

--- src/main/java/com/CICS/Seongnam/Service/Main/MainMapper.java
+++ src/main/java/com/CICS/Seongnam/Service/Main/MainMapper.java
@@ -1,4 +1,12 @@
 package com.CICS.Seongnam.Service.Main;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import java.util.List;
+
+@Mapper
 public interface MainMapper {
+
+    @Select("SELECT Archive_ID FROM Archives_System_MetaData WHERE Archive_ID = '1'")
+    String getallArchivesID();
 }
---
+++ src/main/java/com/CICS/Seongnam/Service/Main/MainService.java
@@ -0,0 +1,17 @@
+package com.CICS.Seongnam.Service.Main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MainService {
+
+    @Autowired
+    private MainMapper mainMapper;
+
+    public String getallArchivesID() throws Exception {
+        return mainMapper.getallArchivesID();
+    }
+}
