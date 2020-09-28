3dfb508ddfe4cad9e1490db6579e064488e2740d
notRoyKim <corgyeier@gmail.com>
2020-09-28 17:51:52 +0900

    main randslider

--- src/main/java/com/CICS/Seongnam/Service/Main/MainMapper.java
+++ src/main/java/com/CICS/Seongnam/Service/Main/MainMapper.java
@@ -1,10 +1,13 @@
 package com.CICS.Seongnam.Service.Main;
 
+import com.CICS.Seongnam.Domain.ViewData;
 import org.apache.ibatis.annotations.*;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface MainMapper {
-    @Select("SELECT Archive_ID FROM Archives_System_MetaData")
-    String[] getallArchivesID();
+
+    @Select("SELECT Archive_ID, Title, Main_Contents, Content_Media FROM Archives_System_MetaData ORDER BY RAND() LIMIT 4;")
+    List<ViewData> getMainRandSlider();
 }
