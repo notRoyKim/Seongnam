73150bcfb557de11ece7a14050c5e3b415585e01
notRoyKim <corgyeier@gmail.com>
2020-10-08 15:32:42 +0900

    mainslider 관리(sql 쿼리 수정해야함)

--- src/main/java/com/CICS/Seongnam/Service/Main/MainMapper.java
+++ src/main/java/com/CICS/Seongnam/Service/Main/MainMapper.java
@@ -1,5 +1,6 @@
 package com.CICS.Seongnam.Service.Main;
 
+import com.CICS.Seongnam.Domain.Main_Rand_Slide;
 import com.CICS.Seongnam.Domain.ViewData;
 import com.CICS.Seongnam.Domain.View_Data_Info;
 import org.apache.ibatis.annotations.*;
@@ -8,9 +9,6 @@ import java.util.Map;
 
 @Mapper
 public interface MainMapper {
-    @Select("SELECT No, Title, Contents FROM Data_Info ORDER BY RAND() LIMIT 4;")
-    List<View_Data_Info> getMainRandSliderRight();
-
-    @Select("SELECT Files FROM FileEvent WHERE Data_No = '${No}';")
-    String getMainRandSliderLeft();
+    @Select("SELECT No, Title, Contents, Files FROM Data_Info,FileEvent ORDER BY Rand() LIMIT 4;")
+    List<Main_Rand_Slide> getMainRandSlide();
 }
--- src/main/java/com/CICS/Seongnam/Service/Main/MainService.java
+++ src/main/java/com/CICS/Seongnam/Service/Main/MainService.java
@@ -3,6 +3,7 @@ package com.CICS.Seongnam.Service.Main;
 import com.CICS.Seongnam.Domain.Main_Rand_Slide;
 import com.CICS.Seongnam.Domain.ViewData;
 import com.CICS.Seongnam.Domain.View_Data_Info;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +15,16 @@ public class MainService {
     @Autowired
     private MainMapper mainMapper;
 
+    public List<Main_Rand_Slide> getMainRandSlide() {
+        List<Main_Rand_Slide> tempList = new ArrayList<>();
+        tempList = mainMapper.getMainRandSlide();
 
+        for(int i = 0; i < 4 ; i++) {
+            Main_Rand_Slide tempRandSlideObject = tempList.get(i);
+            tempRandSlideObject.setFiles(tempRandSlideObject.getFiles().split(",")[0]);
+            tempList.set(i,tempRandSlideObject);
+        }
+
+        return tempList;
+    }
 }
