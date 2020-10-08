f914d70817b854c962d6719ac4b5434181aabece
NotRoyKim <sinant7616@gmail.com>
2020-10-08 16:09:11 +0900

    mainslide 기능 구현

--- src/main/java/com/CICS/Seongnam/Service/Main/MainMapper.java
+++ src/main/java/com/CICS/Seongnam/Service/Main/MainMapper.java
@@ -1,14 +1,11 @@
 package com.CICS.Seongnam.Service.Main;
 
 import com.CICS.Seongnam.Domain.Main_Rand_Slide;
-import com.CICS.Seongnam.Domain.ViewData;
-import com.CICS.Seongnam.Domain.View_Data_Info;
 import org.apache.ibatis.annotations.*;
 import java.util.List;
-import java.util.Map;
 
 @Mapper
 public interface MainMapper {
-    @Select("SELECT No, Title, Contents, Files FROM Data_Info,FileEvent ORDER BY Rand() LIMIT 4;")
+    @Select("SELECT * FROM MainRandSlideView ORDER BY Rand() LIMIT 4;")
     List<Main_Rand_Slide> getMainRandSlide();
 }
